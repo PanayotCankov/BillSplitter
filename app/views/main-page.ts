e79ebe37cb28a52345af308feb43cde11765ef3e
Kristina Koeva <kristinalkoeva@gmail.com>
2015-08-04 12:24:27 +0300

    Add button to the action bar that would allow you to take a picture

--- app/views/main-page.ts
+++ app/views/main-page.ts
@@ -1,10 +1,17 @@
 import observable = require("data/observable");
 import pages = require("ui/page");
 import gestures = require("ui/gestures");
+ import cameraModule = require("camera");
 
 // Event handler for Page "loaded" event attached in main-page.xml
 export function pageLoaded(args: observable.EventData) {
     // Get the event sender
     var page = <pages.Page>args.object;
     // page.bindingContext = null;
+}
+
+export function addImageButtonTap() {
+    cameraModule.takePicture().then(picture => {
+        console.log("Result is an image source instance");
+    });
 }
