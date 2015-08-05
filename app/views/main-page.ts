b5b75f932b53214046c464f5ffff34c666f0f426
vakrilov <alexander.vakrilov@telerik.com>
2015-08-05 10:45:38 +0300

    Selection for android

--- app/views/main-page.ts
+++ app/views/main-page.ts
@@ -6,22 +6,35 @@ import grid = require("ui/layouts/grid-layout");
 import absolute = require("ui/layouts/absolute-layout");
 import imageSource = require("image-source");
 import image = require("ui/image");
+import paltform = require("ui/image");
+import utils = require("utils/utils");
 
 var page;
 var billImageView: image.Image;
 var container: absolute.AbsoluteLayout;
 var selection: grid.GridLayout;
 var croppedImage: image.Image;
+var density: number;
+
 // Event handler for Page "loaded" event attached in main-page.xml
 export function pageLoaded(args: observable.EventData) {
     // Get the event sender
     page = <pages.Page>args.object;
     // page.bindingContext = null;
+    density = utils.layout.getDisplayDensity();
     billImageView = <image.Image>page.getViewById("billImageView");
     container = <absolute.AbsoluteLayout>page.getViewById("image-container");
     selection = <grid.GridLayout> page.getViewById("selection");
     croppedImage = <image.Image> page.getViewById("cropped-image");
-    container.observe(gestures.GestureTypes.pan, containerPan);
+
+    if (container.android) {
+        container.android.setOnTouchListener(new android.view.View.OnTouchListener({
+            onTouch: containerTouch
+        }));
+    }
+    else if (container.ios) {
+        container.observe(gestures.GestureTypes.pan, containerPan);
+    }
 }
 
 
@@ -33,20 +46,42 @@ export function addImageButtonTap() {
 }
 
 // Selection
-function containerPan(args: gestures.PanGestureEventData) {
-    switch (args.ios.state) {
-        case UIGestureRecognizerState.UIGestureRecognizerStateBegan:
-            var point = args.ios.locationInView(args.view.ios);
-            panStarted(point.x, point.y);
+function containerTouch(view: android.view.View, motionEvent: android.view.MotionEvent) {
+    var x = motionEvent.getX() / density;
+    var y = motionEvent.getY() / density;
+    switch (motionEvent.getAction()) {
+        case android.view.MotionEvent.ACTION_DOWN:
+            panStarted(x, y);
             break;
 
-        case UIGestureRecognizerState.UIGestureRecognizerStateChanged:
-            panMoving(args.deltaX, args.deltaY);
+        case android.view.MotionEvent.ACTION_MOVE:
+            panMoving(x - selX, y - selY);
             break;
 
-        case UIGestureRecognizerState.UIGestureRecognizerStateEnded:
+        case android.view.MotionEvent.ACTION_UP:
             panEnded();
             break;
+    }
+
+    return true;
+}
+
+function containerPan(args: gestures.PanGestureEventData) {
+    if (args.ios) {
+        switch (args.ios.state) {
+            case UIGestureRecognizerState.UIGestureRecognizerStateBegan:
+                var point = args.ios.locationInView(args.view.ios);
+                panStarted(point.x, point.y);
+                break;
+
+            case UIGestureRecognizerState.UIGestureRecognizerStateChanged:
+                panMoving(args.deltaX, args.deltaY);
+                break;
+
+            case UIGestureRecognizerState.UIGestureRecognizerStateEnded:
+                panEnded();
+                break;
+        }
     }
 }
 
@@ -79,8 +114,7 @@ function updateSelection() {
     var left = selX - (selW > 0 ? 0 : -selW);
     var top = selY - (selH > 0 ? 0 : -selH)
 
-    console.log("selection[" + left + ", " + top + ", " + selW + ", " + selH + "]");
-
+    //console.log("selection[" + left + ", " + top + ", " + selW + ", " + selH + "]");
     absolute.AbsoluteLayout.setTop(selection, top);
     absolute.AbsoluteLayout.setLeft(selection, left);
     selection.width = Math.abs(selW);
@@ -93,16 +127,21 @@ function cropImage() {
         return;
     }
 
-    var left = selX - (selW > 0 ? 0 : -selW);
-    var top = selY - (selH > 0 ? 0 : -selH);
-    var width = Math.abs(selW);
-    var height = Math.abs(selH);
-    var rect = CGRectMake(left, top, width, height); 
- 
-    // Create bitmap image from original image data,
-    // using rectangle to specify desired crop area
-    var imageRef = CGImageCreateWithImageInRect(billImageView.ios.image.CGImage, rect);
-    var uiImage = UIImage.imageWithCGImage(imageRef);
-    croppedImage.imageSource = imageSource.fromNativeSource(uiImage);
-    CGImageRelease(imageRef);
+    var left = (selX - (selW > 0 ? 0 : -selW)) * density;
+    var top = (selY - (selH > 0 ? 0 : -selH)) * density;
+    var width = (Math.abs(selW)) * density;
+    var height = (Math.abs(selH)) * density;
+
+    if (billImageView.ios) {
+        var rect = CGRectMake(left, top, width, height);
+        var imageRef = CGImageCreateWithImageInRect(billImageView.ios.image.CGImage, rect);
+        var uiImage = UIImage.imageWithCGImage(imageRef);
+        croppedImage.imageSource = imageSource.fromNativeSource(uiImage);
+        CGImageRelease(imageRef);
+    }
+    else if (imgSrc.android) {
+        var croppedBitmap = android.graphics.Bitmap.createBitmap(imgSrc.android, left, top, width, height)
+        var croppedSource = imageSource.fromNativeSource(croppedBitmap);
+        croppedImage.imageSource = croppedSource;
+    }
 }
